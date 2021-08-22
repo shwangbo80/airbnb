2490ef87a2a7669318ff042da2df6363b16ac38e
Omar Yazbek <rott.artwork@gmail.com>
2021-08-22 12:34:17 -0700

    card data added

---
+++ src/client/components/ExploreNearby/ExploreCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import exploreData from './ExploreData';
+
+function ExploreCard(props) {
+  return (
+    <div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default ExploreCard;
---
+++ src/client/components/ExploreNearby/ExploreData.js
@@ -0,0 +1,52 @@
+const exploreData = [
+  {
+    id: 1,
+    destination: 'Los Angeles',
+    image: './img/Los-Angeles.png',
+    distance: '30 minute drive',
+  },
+  {
+    id: 2,
+    destination: 'San Diego',
+    image: './img/San-Diego.png',
+    distance: '3 hour drive',
+  },
+  {
+    id: 3,
+    destination: 'Las Vegas',
+    image: './img/Los-Vegas.png',
+    distance: '5.5 hour drive',
+  },
+  {
+    id: 4,
+    destination: 'Henderson',
+    image: './img/Henderson.png',
+    distance: '5.5 hour drive',
+  },
+  {
+    id: 5,
+    destination: 'Paradise',
+    image: './img/Paradise.png',
+    distance: '5.5 hour drive',
+  },
+  {
+    id: 6,
+    destination: 'Big Bear Lake',
+    image: './img/Big-Bear-Lake.png',
+    distance: '2.5 hour drive',
+  },
+  {
+    id: 7,
+    destination: 'Palm Springs',
+    image: './img/Palm-Springs.png',
+    distance: '2.5 hour drive',
+  },
+  {
+    id: 8,
+    destination: 'Anaheim',
+    image: './img/Anaheim.png',
+    distance: '1 hour drive',
+  },
+];
+
+export default exploreData;
