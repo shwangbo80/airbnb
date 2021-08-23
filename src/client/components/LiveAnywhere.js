8fdf208e617cd6cb3888ba5887338eb20e80d00e
kevinhtran <kevdeveloper7@gmail.com>
2021-08-23 13:55:12 -0700

    imported images

--- src/client/components/LiveAnywhere.js
+++ src/client/components/LiveAnywhere.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import img1 from '../../assets/images/liveanywhere-1.jpg'
+import img2 from '../../assets/images/liveanywhere-2.jpg'
+import img3 from '../../assets/images/liveanywhere-3.jpg'
+import img4 from '../../assets/images/liveanywhere-4.jpg'
 
 const LiveAnywhere = () => {
   return (
-    <div>
-      <h3>LiveAnywhere</h3>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Col xs={6} md={3}>
+            <Image src={img1} rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src={img2} rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src={img3} rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src={img4} rounded />
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
