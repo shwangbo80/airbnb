031fa519b1a31701350739e4df5601c2dba69754
kevinhtran <kevdeveloper7@gmail.com>
2021-09-14 17:15:53 -0700

    commit

--- src/client/components/DiscoverThingsToDo.js
+++ src/client/components/DiscoverThingsToDo.js
@@ -9,3 +9,4 @@ const DiscoverThingsToDo = () => {
 }
 
 export default DiscoverThingsToDo
+w
--- src/client/components/TryHosting.js
+++ src/client/components/TryHosting.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const TryHosting = () => {
+  yarn
   return (
     <div>
       <h3>TryHosting</h3>
